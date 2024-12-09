6b368fb9eca0c4e038db5bd95d1af2ba0a4c6649
Pacocc97 <pacocc97@gmail.com>
2024-12-08 20:49:33 -0600

    cambio

--- medusa-config.ts
+++ medusa-config.ts
@@ -3,19 +3,6 @@ import { loadEnv, defineConfig } from '@medusajs/framework/utils';
 loadEnv(process.env.NODE_ENV || 'development', process.cwd());
 
 module.exports = defineConfig({
-  admin: {
-    vite: (config) => {
-      return {
-        ...config,
-        server: {
-          ...config.server,
-          hmr: {
-            port: 9000, // Set the port you want to use here
-          },
-        },
-      };
-    },
-  },
   projectConfig: {
     databaseUrl: process.env.DATABASE_URL,
     workerMode:
