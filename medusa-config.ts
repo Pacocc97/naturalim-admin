3afb596dc030fbb5eb0f734f4da659435b69ca9a
Pacocc97 <pacocc97@gmail.com>
2024-12-08 20:45:14 -0600

    basioc

--- medusa-config.ts
+++ medusa-config.ts
@@ -10,7 +10,7 @@ module.exports = defineConfig({
         server: {
           ...config.server,
           hmr: {
-            port: 443, // Set the port you want to use here
+            port: 9000, // Set the port you want to use here
           },
         },
       };
